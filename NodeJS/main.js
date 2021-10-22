d5e48ea42525e494bdcbbd17c3aa64843f97408a
Arsalan <67230071+arsalanfiroozi@users.noreply.github.com>
2021-10-22 21:43:34 +0330

    .

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -4,22 +4,14 @@ const port = 3000
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const crypto = require('crypto');
-const hash = crypto.createHash('sha256');
 
 app.post('/node', async(req, res) => {
-  /*
-  const { Client } = require('pg')
-  const client = new Client()
-  await client.connect()
-  const res2 = await client.query('SELECT * FROM "Students"')
-  console.log(res2.rows[0].name) // Hello world!
-  res.send(res2.rows[0].name)
-  */
   //res.send('Hello World!')
   //res.send(req.body.Input1)
   //const hash = crypto.createHash('sha256').update(pwd).digest('base64');
   //await client.end()
 
+  const hash = crypto.createHash('sha256');
   //passing the data to be hashed
   data = hash.update(req.body.Input1, 'utf-8');
   //Creating the hash in the required format
@@ -27,6 +19,20 @@ app.post('/node', async(req, res) => {
   //Printing the output on the console
   console.log("hash : " + gen_hash);
   res.send("hash : " + gen_hash);
+
+  const { Client } = require('pg')
+  const client = new Client({
+    user: 'postgres',
+    password: 'Arsalan995384',
+    database: 'DB_First'
+  })
+  await client.connect()
+  const res2 = await client.query('SELECT * FROM "Train" WHERE "Hash"=\''+gen_hash+'\'')
+  if(res2.rows.length === 0){
+    const res3 = await client.query('SELECT id FROM "Train" ORDER BY id DESC LIMIT 1')
+    const res4 = await client.query('INSERT INTO "Train"(id,"Hash") VALUES ('+res3.rows[0].id+',\''+gen_hash+'\') ')    
+  }
+  await client.end()
 })
 
 app.listen(port, () => {
