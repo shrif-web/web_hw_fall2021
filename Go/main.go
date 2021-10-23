c4659db8ddfa4d80b295deb9cd871a5d033a560f
Arsalan <67230071+arsalanfiroozi@users.noreply.github.com>
2021-10-23 09:04:27 +0330

    Go

--- Go/main.go
+++ Go/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"fmt"
 	"log"
 	"net/http"
 
@@ -13,21 +15,47 @@ func main() {
 	r := gin.Default()
 	r.GET("/go", func(c *gin.Context) {
 		Input := c.Query("Input1") // shortcut for c.Request.URL.Query().Get("lastname")
+
 		//PGUSER=postgres PGPASSWORD=Arsalan995384 PGDATABASE=DB_First PGPORT=5432
-		connStr := "user=postgres password=Arsalan995384 dbname=DB_First"
+		connStr := "user=postgres password=Arsalan995384 sslmode=disable dbname=DB_First"
 		db, err := sql.Open("postgres", connStr)
 		if err != nil {
 			log.Fatal(err)
 		}
 		//rows, err := db.Query("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '$1'", Input)
 		var Key string
-		db.QueryRow("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '$1'", Input).Scan(&Key)
-		c.String(http.StatusOK, "Hello %s", Key[0])
+		//rows, err := db.Query("SELECT name FROM users WHERE age = $1", age)
+		err2 := db.QueryRow("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '" + Input + "'").Scan(&Key)
+		if err2 != nil {
+			c.String(http.StatusOK, "No Record Found!")
+			//log.Fatal(err2)
+		} else {
+			c.String(http.StatusOK, "Hello %s", Key)
+		}
+	})
+	r.POST("/go", func(c *gin.Context) {
+		Input := c.PostForm("Input1") // shortcut for c.Request.URL.Query().Get("lastname")
+		h := sha256.New()
+		h.Write([]byte(Input))
+
+		//PGUSER=postgres PGPASSWORD=Arsalan995384 PGDATABASE=DB_First PGPORT=5432
+		connStr := "user=postgres password=Arsalan995384 sslmode=disable dbname=DB_First"
+		db, err := sql.Open("postgres", connStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+		//rows, err := db.Query("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '$1'", Input)
+		var Key string
+		//rows, err := db.Query("SELECT name FROM users WHERE age = $1", age)
+		err2 := db.QueryRow("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '" + fmt.Sprintf("%x", h.Sum(nil)) + "'").Scan(&Key)
+		if err2 != nil {
+			err3 := db.QueryRow("INSERT INTO \"Train\"(\"Key\",\"Hash\") VALUES ('" + Input + "','" + fmt.Sprintf("%x", h.Sum(nil)) + "')")
+			if err2 != nil {
+				log.Print(err3.Err())
+			}
+		}
+		c.String(http.StatusOK, "%s", fmt.Sprintf("%x", h.Sum(nil)))
 	})
 	r.Run(":9090")
 
 }
-
-/*
-
- */
