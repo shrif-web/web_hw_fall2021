2ab298c6446a70f42b1ee7767aa8a4f9a3500961
Arsalan <67230071+arsalanfiroozi@users.noreply.github.com>
2021-10-23 22:30:34 +0330

    .

--- Go/main.go
+++ Go/main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"crypto/sha256"
 	"database/sql"
+
 	//"fmt"
+	b64 "encoding/base64"
 	"log"
 	"net/http"
-	b64 "encoding/base64"
+
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
@@ -19,20 +21,16 @@ func NewSHA256(data []byte) []byte {
 func main() {
 	r := gin.Default()
 	r.GET("/go", func(c *gin.Context) {
-	//r.GET("/node", func(c *gin.Context) {	
-		Input := c.Query("Input1") // shortcut for c.Request.URL.Query().Get("lastname")
-		//PGUSER=postgres PGPASSWORD=Arsalan995384 PGDATABASE=DB_First PGPORT=5432
-		// connStr := "user=postgres password=Arsalan995384 sslmode=disable dbname=DB_First"
-		connStr := "user=postgres password=amir1379 sslmode=disable dbname=My_db"
+		Input := c.Query("Input1")
+		connStr := "user=postgres password=Arsalan995384 sslmode=disable dbname=DB_First"
+		//connStr := "user=postgres password=amir1379 sslmode=disable dbname=My_db"
 		db, err := sql.Open("postgres", connStr)
 		if err != nil {
 			log.Fatal(err)
 		}
-		//rows, err := db.Query("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '$1'", Input)
 		var Key string
-		//rows, err := db.Query("SELECT name FROM users WHERE age = $1", age)
 		err2 := db.QueryRow("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '" + Input + "'").Scan(&Key)
-		if err2 != nil {
+		if err2 == nil {
 			c.String(http.StatusOK, "No Record Found!")
 			//log.Fatal(err2)
 		} else {
@@ -40,42 +38,25 @@ func main() {
 		}
 	})
 	r.POST("/go", func(c *gin.Context) {
-	//r.POST("/node", func(c *gin.Context) {		
 		Key := c.PostForm("Input1") // shortcut for c.Request.URL.Query().Get("lastname")
-		// deleted
-		// h := sha256.New()
-		// new 
-		// hash := sha256.Sum256(Key)
-		// hash_str := b64.StdEncoding.EncodeToString(hash)
 		hash := b64.StdEncoding.EncodeToString(NewSHA256([]byte(Key)))
-		//
-		// deleted
-		// h.Write([]byte(Input))
-
-		//PGUSER=postgres PGPASSWORD=Arsalan995384 PGDATABASE=DB_First PGPORT=5432
-		// connStr := "user=postgres password=Arsalan995384 sslmode=disable dbname=DB_First"
-		connStr := "user=postgres password=amir1379 sslmode=disable dbname=My_db"
+		connStr := "user=postgres password=Arsalan995384 sslmode=disable dbname=DB_First"
+		//connStr := "user=postgres password=amir1379 sslmode=disable dbname=My_db"
 		db, err := sql.Open("postgres", connStr)
 		if err != nil {
 			log.Fatal(err)
 			//log.Print(err)
 		}
-		//rows, err := db.Query("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '$1'", Input)
 		var Key_t string
-		//rows, err := db.Query("SELECT name FROM users WHERE age = $1", age)
-		// err2 := db.QueryRow("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '" + fmt.Sprintf("%x", h.Sum(nil)) + "'").Scan(&Key)
 		err2 := db.QueryRow("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '" + hash + "'").Scan(&Key_t)
 		if err2 != nil {
-			// err3 := db.QueryRow("INSERT INTO \"Train\"(\"Key\",\"Hash\") VALUES ('" + Input + "','" + fmt.Sprintf("%x", h.Sum(nil)) + "')")
 			err3 := db.QueryRow("INSERT INTO \"Train\"(\"Key\",\"Hash\") VALUES ('" + Key + "','" + hash + "')")
-			// if err2 != nil {
-			if err3 != nil {
+			if err3 == nil {
 				// log.Print(err2.Err())
 				log.Fatal(err3)
 			}
 		}
-		// c.String(http.StatusOK, "%s", fmt.Sprintf("%x", h.Sum(nil)))
-		c.String(http.StatusOK, "%s",  hash )
+		c.String(http.StatusOK, "%s", hash)
 	})
 	r.Run(":9090")
 }
