d6f6ffb6dc2f3e76399d03efe07780b0ecaae321
Erfan Nosrati <erfannosrati20@gmail.com>
2021-10-24 16:06:01 +0330

    main.go cors

--- Go/main.go
+++ Go/main.go
@@ -11,8 +11,34 @@ import (
 	_ "github.com/lib/pq"
 )
 
+func CORS(c *gin.Context) {
+
+	// First, we add the headers with need to enable CORS
+	// Make sure to adjust these headers to your needs
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Methods", "*")
+	c.Header("Access-Control-Allow-Headers", "*")
+	c.Header("Content-Type", "application/json")
+
+	// Second, we handle the OPTIONS problem
+	if c.Request.Method != "OPTIONS" {
+
+		c.Next()
+
+	} else {
+
+		// Everytime we receive an OPTIONS request,
+		// we just return an HTTP 200 Status Code
+		// Like this, Angular can now do the real
+		// request using any other method than OPTIONS
+		c.AbortWithStatus(http.StatusOK)
+	}
+}
+
 func main() {
 	r := gin.Default()
+	r.Use(CORS)
+
 	r.GET("/go", func(c *gin.Context) {
 		Input := c.Query("Input1")
 
@@ -79,5 +105,6 @@ func main() {
 		}
 
 	})
-	r.Run(":9091")
+
+	r.Run(":9090")
 }
