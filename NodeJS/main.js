991d0d979b567ebbcdfdbdeb238ccee495831eba
Erfan Nosrati <erfannosrati20@gmail.com>
2021-10-24 15:19:41 +0330

    node js json added

--- NodeJS/main.js
+++ NodeJS/main.js
@@ -18,16 +18,26 @@ const port = config_data.port
 // This function get a string as a key and calculate it's hash and
 // if the hash doesn't exist save the key and the hash in the database
 app.post('/node', async(req, res) => {
+
+  if (req.body.Input1.length < 8){
+    res.json({response: "Input needs to be 8 character at least!"});
+    return
+  }
+
   //distinguish the hash tupe
   const hash = crypto.createHash('sha256');
+
   //passing the data to be hashed
   data = hash.update(req.body.Input1, 'utf-8');
+
   //Creating the hash in the required format
   gen_hash= data.digest('hex');
+
   //Printing the output on the console
   console.log("hash : " + gen_hash);
+
   //Send the hash as the response
-  res.send("hash : " + gen_hash);
+  res.json({response: gen_hash});
 
   // fill out the connection string 
   const { Client } = require('pg')
@@ -58,9 +68,9 @@ app.get('/node', async(req, res) => {
     const query_result = await client.query('SELECT "Key" FROM public."Train" where "Hash" = \''+ req.query.Input1 + '\'')
     if(query_result.rows.length === 0){
       // const res4 = await client.query('INSERT INTO "Train"("Key","Hash") VALUES (\'' + req.params.Input1 + '\',\''+gen_hash+'\')')    
-        res.send('No record!')
+        res.json({response: "No record found!"})
     } else{
-        res.send(query_result.rows[0].Key)
+        res.json({response: query_result.rows[0].Key})
     }
     await client.end()
 })
