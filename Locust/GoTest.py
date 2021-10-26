67db92262176c0aadd9748844fff151cb106107f
Arsalan <67230071+arsalanfiroozi@users.noreply.github.com>
2021-10-26 16:49:02 +0330

    .

--- Locust/GoTest.py
+++ Locust/GoTest.py
@@ -1,9 +1,17 @@
 from locust import HttpUser, task
 import random
+import json
 
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
         n = random.randint(0,100000)
-        In = 12345678945600000 + n
-        self.client.get("/go/sha?Input1="+str(In))
+        In = 12345578945600000 + n
+        # self.client.get("/node/sha256?Input1="+str(In))
+        data = {'Input1': str(In)}
+        self.client.post(
+            url="/go/sha256",
+            data=json.dumps(data),
+            auth=None,
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
--- Locust/NodeTest.py
+++ Locust/NodeTest.py
@@ -1,9 +1,17 @@
 from locust import HttpUser, task
 import random
+import json
 
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
         n = random.randint(0,100000)
         In = 12345178945600000 + n
-        self.client.get("/node/sha?Input1="+str(In))
+        # self.client.get("/node/sha256?Input1="+str(In))
+        data = {'Input1': str(In)}
+        self.client.post(
+            url="/node/sha256",
+            data=json.dumps(data),
+            auth=None,
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
