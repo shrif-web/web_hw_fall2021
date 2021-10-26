e8a7dbf41ec9b3a55ec591bfd7919b8b6c6478aa
Arsalan <67230071+arsalanfiroozi@users.noreply.github.com>
2021-10-26 20:58:10 +0330

    .

--- Go/main.go
+++ Go/main.go
@@ -40,8 +40,10 @@ func main() {
 	r.Use(CORS)
 
 	r.GET("/go", func(c *gin.Context) {
+		// Query is the input by get. if not available Input would be empty.(No Error)
 		Input := c.Query("Input1")
 
+		// Check if the length is below 8 characters
 		if len(Input) < 8 {
 			c.JSON(http.StatusOK, gin.H{
 				"response": "Input needs to be 8 character at least!",
@@ -49,6 +51,7 @@ func main() {
 			return
 		}
 
+		// Connection String
 		connStr := "host=www.pg.com user=admin password=admin sslmode=disable"
 		db, err := sql.Open("postgres", connStr)
 		if err != nil {
@@ -56,7 +59,7 @@ func main() {
 		}
 
 		var rows_db sql.NullInt64
-
+		// Check if the database is not created. if so create the database and the table
 		db.QueryRow("SELECT count(*) AS result FROM pg_database WHERE datname='test-db'").Scan(&rows_db)
 		log.Print("number of rows" + fmt.Sprint(rows_db.Int64))
 		if rows_db.Int64 == 0 {
@@ -66,6 +69,7 @@ func main() {
 
 		var Key sql.NullString
 
+		// fetch key corresponding to hash code from database
 		err2 := db.QueryRow("SELECT \"Key\" FROM public.\"Train\" where \"Hash\" = '" + Input + "'").Scan(&Key)
 		if err2 != nil {
 			c.JSON(http.StatusOK, gin.H{
@@ -81,8 +85,10 @@ func main() {
 	})
 
 	r.POST("/go", func(c *gin.Context) {
+		// Input in post request is achieved by PostForm
 		Key := c.PostForm("Input1") // shortcut for c.Request.URL.Query().Get("lastname")
 
+		// Check if the length is below 8 characters
 		if len(Key) < 8 {
 			c.JSON(http.StatusOK, gin.H{
 				"response": "Input needs to be 8 character at least!",
@@ -90,9 +96,11 @@ func main() {
 			return
 		}
 
+		// Create hash code of input
 		h := sha256.New()
 		h.Write([]byte(Key))
 		hash := fmt.Sprintf("%x", h.Sum(nil))
+		// Connection String
 		connStr := "host=www.pg.com user=admin password=admin sslmode=disable"
 
 		db, err := sql.Open("postgres", connStr)
@@ -102,12 +110,14 @@ func main() {
 
 		var rows_db sql.NullInt64
 
+		// Check if the database is not created. if so create the database and the table
 		db.QueryRow("SELECT count(*) AS result FROM pg_database WHERE datname='test-db'").Scan(&rows_db)
 		if rows_db.Int64 == 0 {
 			db.Query("CREATE DATABASE \"test-db\" WITH OWNER = admin ENCODING = 'UTF8' CONNECTION LIMIT = -1;")
 			db.Query("CREATE TABLE public.\"Train\"(\"Key\" character varying,\"Hash\" character varying,PRIMARY KEY (\"Key\"));ALTER TABLE IF EXISTS public.\"Train\" OWNER to admin;")
 		}
 
+		// Check if hash exist in db
 		var Rows sql.NullInt64
 		err1 := db.QueryRow("SELECT count(\"Key\") FROM public.\"Train\" where \"Hash\" = '" + hash + "'").Scan(&Rows)
 		if err1 != nil {
@@ -119,6 +129,7 @@ func main() {
 				"response": hash,
 			})
 		} else {
+			// if hash doesn't exist, Insert hash and key to db
 			err3 := db.QueryRow("INSERT INTO \"Train\"(\"Key\",\"Hash\") VALUES ('" + Key + "','" + hash + "')")
 			if err3 == nil {
 				log.Fatal(err3)
