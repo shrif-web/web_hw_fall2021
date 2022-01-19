3df3cbb7f523200de227df48ffcbfcaf0c1ff5fb
Arsalan <67230071+arsalanfiroozi@users.noreply.github.com>
2022-01-19 23:45:23 +0330

    Final Enshallah

--- Backend/index.js
+++ Backend/index.js
@@ -8,7 +8,7 @@ import rateLimit from 'express-rate-limit';
 
 const limiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 15 minutes
-    max: 1000, // Limit each IP to 100 requests per window (here, per 15 minutes)
+    max: process.env.N_Limit, // Limit each IP to 100 requests per window (here, per 15 minutes)
     standardHeaders: true, // Return rate limit info in the RateLimit-* headers
     legacyHeaders: false, // Disable the X-RateLimit-* headers
     message: '{"successful":false,"message":"Too many requests!"}'
