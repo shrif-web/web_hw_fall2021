299c8346020d8de85da86db654c0120e26dcace2
erfiboy <erfannosrati20@gmail.com>
2022-01-19 19:58:31 +0330

    retry

--- Database/utils/database.js
+++ Database/utils/database.js
@@ -4,15 +4,35 @@ dotenv.config();
 
 // connection string
 const sequelize = new Sequelize('postgres://admin:admin@' + process.env.PG_Path + '/admin')
-try {
-    sequelize.authenticate({
-    }).then(() =>
-        sequelize.sync()
-    )
-}
-catch (err) {
-    console.log("could not connect to the data base!")
-    console.log(err)
+
+let is_connected = false;
+
+async function connect_db(){
+    try {       
+        await sequelize.authenticate()
+        await sequelize.sync()
+        is_connected = true;
+    }
+    catch (err) {
+    }
 }
 
+async function try_to_connect(){
+    while(!is_connected){   
+        try{
+            await connect_db();
+        }
+        catch{
+        }
+        finally{
+            sleep(30000);
+        }
+    }
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+try_to_connect()
+  
 export default sequelize;
